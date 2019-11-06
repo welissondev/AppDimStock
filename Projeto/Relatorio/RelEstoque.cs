6d39078862b410ea4ca0be5894078ae60b098bc8
WelissonDev <welissondev@gmail.com>
2019-11-06 07:51:47 -0300

    Definição das propriedades

--- Projeto/Relatorio/RelEstoque.cs
+++ Projeto/Relatorio/RelEstoque.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace SysEstoque.Relatorio
 {
-    class RelEstoque
+    public class RelEstoque
     {
+        #region Get e Set
+        public int CodigoProduto { get; set; }
+        public int TamanhoProduto { get; set; }
+        public int ReferenciaProduto{ get; set; }
+        public string DescricaoProduto { get; set; }
+        public string FornecedorProduto { get; set; }
+        public int EstoqueMin { get; set; }
+        public int EstoqueMax { get; set; }
+        public int EstoqueQuant { get; set; }
+        public double ValorEmEstoque { get; set; }
+        public string ResumoEstoque { get; set; }
+        public string ResultadoEstoque { get; set; }
+        public List<RelEstoque> Lista { get; set; }
+        #endregion
     }
 }
