d7612f0f0183df860ac8f8d9207e23b93c365201
WelissonDev <welissondev@gmail.com>
2019-11-28 10:06:55 -0300

    Refaturação das propriedades

--- Projeto/Report/ReportStockProduct.cs
+++ Projeto/Report/ReportStockProduct.cs
@@ -7,18 +7,43 @@ namespace DimStock.Report
     public class ReportStockProduct
     {
         #region Get e Set
-        public int ProductCode { get; set; }
-        public int ProductSize { get; set; }
-        public int ProductReference { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductSize { get; set; }
+        public string ProductReference { get; set; }
         public string ProductDescription { get; set; }
-        public string ProductSupplier { get; set; }
-        public int StockMin { get; set; }
-        public int StockMax { get; set; }
+        public string Supplier { get; set; }
+        public int MinStock { get; set; }
+        public int MaxStock { get; set; }
         public int StockQuantity { get; set; }
         public double StockValue { get; set; }
         public string StockResume { get; set; }
         public string StockResult { get; set; }
-        public List<ReportStockProduct> ListOfRecords{ get; set; }
+        public List<ReportStockProduct> ListOfRecords { get; set; }
         #endregion
+
+        public void Generate(List<BllStockProduct> listOfRecords)
+        {
+            ListOfRecords = new List<ReportStockProduct>();
+
+            for (int i = 0; i < listOfRecords.Count; i++)
+            {
+                var reportStockProduct = new ReportStockProduct()
+                {
+                    ProductCode = listOfRecords[i].ProductCode,
+                    ProductSize = listOfRecords[i].ProductSize,
+                    ProductReference = listOfRecords[i].ProductReference,
+                    Supplier = listOfRecords[i].Supplier,
+                    ProductDescription = listOfRecords[i].ProductDescription,
+                    MinStock = listOfRecords[i].MinStock,
+                    MaxStock = listOfRecords[i].MaxStock,
+                    StockQuantity = listOfRecords[i].StockQuantity,
+                    StockValue = listOfRecords[i].StockValue,
+                    StockResume = listOfRecords[i].StockResume,
+                    StockResult = listOfRecords[i].StockResult
+                };
+
+                ListOfRecords.Add(reportStockProduct);
+            }
+        }
     }
 }
