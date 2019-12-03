a3cb7755deaa43218b196d7cf39a2b49922bf000
WelissonDev <welissondev@gmail.com>
2019-12-03 00:25:38 -0300

    Mudança aplicada para classe de notificação da bussiness

--- Projeto/Business/BllNotification.cs
+++ Projeto/Business/BllNotification.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Uma classe statica que recebe uma resposta do model sobre á ultima operação solicitada!
     /// </summary>
-    public static class BllNotificar
+    public static class BllNotification
     {
         public static string Message { get; set; }
         public static bool State { get; set; }
