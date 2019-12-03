04e1fd287256b51ff59c949ffc3c5bcab824c615
WelissonDev <welissondev@gmail.com>
2019-12-03 15:13:12 -0300

    Passando os paramêtros de consulta para gerar relatório

--- Projeto/View/FrmProductList.cs
+++ Projeto/View/FrmProductList.cs
@@ -83,7 +83,14 @@ namespace DimStock.View
         {
             try
             {
-                var product = new BllProduct();
+                var product = new BllProduct()
+                {
+                    Code = TxtQueryByCode.Text,
+                    Size = TxtQueryBySize.Text,
+                    Reference = TxtQueryByReference.Text,
+                    Description = TxtQueryByDescription.Text
+                };
+
                 product.ListAll(); product.GenerateReport();
 
                 var path = "DimStock.Report.RpvProduct.rdlc";
