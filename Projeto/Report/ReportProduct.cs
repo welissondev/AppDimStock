0aa13c58042ad06b2215096b9bdefb0d863aaf26
WelissonDev <welissondev@gmail.com>
2019-12-03 01:04:36 -0300

    Definições das propriedades

--- Projeto/Report/ReportProduct.cs
+++ Projeto/Report/ReportProduct.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DimStock.Business;
 
 namespace DimStock.Report
 {
     public class ReportProduct
     {
-
+        #region Get e Set
+        public int Code { get; set; }
+        public int Size { get; set; }
+        public int Reference { get; set; }
+        public string Supplier { get; set; }
+        public string Description { get; set; }
+        public int MinStock { get; set; }
+        public int MaxStock { get; set; }
+        public double CostPrice { get; set; }
+        public List<BllProduct> ListOfRecords { get; set; }
+        #endregion
     }
 }
