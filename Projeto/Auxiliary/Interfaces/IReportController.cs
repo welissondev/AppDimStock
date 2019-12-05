23bfdd5d6e8ed85cf757e3327415c61719cacf38
WelissonDev <welissondev@gmail.com>
2019-12-05 18:39:46 -0300

    IReportController adicionada

---
+++ Projeto/Auxiliary/Interfaces/IReportController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DimStock.Auxiliary.Interfaces
+{
+    public interface IReportController<T>
+    {
+        void GenerateReport(List<T> listOfRecords);
+    }
+}
