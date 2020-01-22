7d4085368617c617bcc5f76cf55021101c70f2f3
WelissonDev <welissondev@gmail.com>
2020-01-22 13:33:23 -0300

    Adiciona uma classe de empresa

---
+++ Projeto/Business/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimStock.Business
+{
+    public class Company
+    {
+        public int Id { get; set; }    
+        public string Name { get; set; }
+        public string CPF_CNPJ{ get; set; }
+        public string RG_IE { get; set; }
+        public string AddRess { get; set; }
+        public string District{ get; set; }
+        public string City{ get; set; }
+        public string State { get; set; }
+        public string Cep { get; set; }
+        public int StrieetNumber{ get; set; }
+    }
+}
