9c7b8e87fe4bd5e8892e2e858b657a8e41fb29bb
WelissonDev <welissondev@gmail.com>
2020-02-04 00:50:08 -0300

    Adiciona classe de categoria do produto

---
+++ Projeto/Business/ProductCategory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using DimStock.Auxiliarys;
+
+namespace DimStock.Business
+{
+    public class ProductCategory
+    {
+        public ProductCategory(){}
+
+        public ProductCategory(AxlDataPagination dataPagination)
+        {
+            DataPagination = dataPagination;
+        }
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public List<ProductCategory> ListOfRecords { get; set; }
+        public AxlDataPagination DataPagination { get; set; }
+
+        public bool Register()
+        {
+            return false;
+        }
+
+        public bool Modify(int id)
+        {
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            return false;
+        }
+
+        public void SearchData()
+        {
+
+        }
+    }
+}
