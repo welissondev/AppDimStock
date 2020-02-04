fffe353e73e93669f5b567023d0baab491aedee6
WelissonDev <welissondev@gmail.com>
2020-02-04 01:06:42 -0300

    Implementa o método de cadastro da categoria

--- Projeto/Business/ProductCategory.cs
+++ Projeto/Business/ProductCategory.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
 using DimStock.Auxiliarys;
 
 namespace DimStock.Business
 {
     public class ProductCategory
     {
-        public ProductCategory(){}
+        public ProductCategory() { }
 
         public ProductCategory(AxlDataPagination dataPagination)
         {
@@ -19,7 +22,49 @@ namespace DimStock.Business
 
         public bool Register()
         {
-            return false;
+            var registerState = false;
+            var sqlCommand = string.Empty;
+
+            using (var connection = new Connection())
+            {
+                using (connection.Transaction = connection.Open().BeginTransaction())
+                {
+                    sqlCommand = @"INSERT INTO ProductCategory
+                    (Description)VALUES(@Description)";
+
+                    connection.AddParameter("@Description", 
+                    OleDbType.VarChar, Description);
+
+                    registerState = 
+                    connection.ExecuteTransaction(
+                    sqlCommand) > 0;
+
+                    //Pegar id do último registro inserido
+                    Id = Convert.ToInt32(connection.ExecuteScalar(
+                    "SELECT MAX(Id) From ProductCategory"));
+
+                    //Registrar histórico do usuário
+                    var userHistory = new UserHistory(connection)
+                    {
+                        UserId = AxlLogin.Id,
+                        OperationType = "Cadastrou",
+                        OperationModule = "Produto Categoria",
+                        OperationDate = Convert.ToDateTime(DateTime.Now.ToString("dd-MM-yyyy")),
+                        OperationHour = DateTime.Now.ToString("HH:mm:ss"),
+                        AffectedFields = GetAffectedFields(Id, connection)
+                    };
+
+                    registerState = userHistory.Register();
+
+                    //Finalizar transação
+                    connection.Transaction.Commit();
+
+                    AxlMessageNotifier.Message = "Categoria " +
+                    "cadastrada com sucesso!";
+                }
+
+                return registerState;
+            }
         }
 
         public bool Modify(int id)
@@ -35,6 +80,25 @@ namespace DimStock.Business
         public void SearchData()
         {
 
+        }
+
+        public string GetAffectedFields(int id, Connection connection)
+        {
+            var affectedFieldList = new List<string>();
+
+            var commandSQL = @"SELECT * From ProductCategory Where Id = @Id";
+            connection.AddParameter("@Id", OleDbType.Integer, id);
+
+            using (var dataReader = connection.QueryWithDataReader(commandSQL))
+            {
+                while (dataReader.Read())
+                {
+                    affectedFieldList.Add("Id:" + dataReader["Id"].ToString());
+                    affectedFieldList.Add("Descrição:" + dataReader["Description"].ToString());
+                }
+            }
+
+            return string.Join(" | ", affectedFieldList.Select(x => x.ToString()));
         }
     }
 }
