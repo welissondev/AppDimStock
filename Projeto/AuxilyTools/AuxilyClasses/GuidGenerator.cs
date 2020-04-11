5af60ba3cf9cc78bcda659d8c4767f5ddb830e39
WelissonDev <welissondev@gmail.com>
2020-04-11 20:27:00 -0300

    Adiciona classe gerador de guid

---
+++ Projeto/AuxilyTools/AuxilyClasses/GuidGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DimStock.AuxilyTools.AuxilyClasses
+{
+    public class GuidGenerator
+    {
+        public string GetGuid(bool withCharacter = false)
+        {
+            var guidGenerated = string.Empty;
+
+            switch (withCharacter)
+            {
+                case true:
+                    guidGenerated = Guid.NewGuid().ToString();
+                    break;
+
+                case false:
+                    guidGenerated = Guid.NewGuid().ToString().Replace("{", "").Replace("}", "").Replace("-", "");
+                    break;
+            }
+            return guidGenerated;
+        }
+    }
+}
