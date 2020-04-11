c84756c260101845e25210ea17578131c81e2a14
WelissonDev <welissondev@gmail.com>
2020-04-11 17:16:17 -0300

    Adiciona metodo gerador do código

--- Projeto/AuxilyTools/AuxilyClasses/SingleCodeGenerator.cs
+++ Projeto/AuxilyTools/AuxilyClasses/SingleCodeGenerator.cs
@@ -1,6 +1,13 @@
-﻿namespace DimStock.AuxilyTools.AuxilyClasses
+﻿using System;
+
+namespace DimStock.AuxilyTools.AuxilyClasses
 {
     public class SingleCodeGenerator
     {
+        public static int GetNumber(int numberRef)
+        {
+            var random = new Random(numberRef);
+            return random.Next();
+        }
     }
 }
