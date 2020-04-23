0cc1804f44820ebde9a0f7e35b0a19d893b5254b
WelissonDev <welissondev@gmail.com>
2020-04-23 14:23:43 -0300

    Adiciona opção para definir o icone do notificador de menssagem

--- Projeto/AuxilyTools/AuxilyClasses/MessageNotifier.cs
+++ Projeto/AuxilyTools/AuxilyClasses/MessageNotifier.cs
@@ -10,21 +10,33 @@ namespace DimStock.AuxilyTools.AuxilyClasses
         public static string Message { get; private set; }
         public static string Title { get; private set; }
 
-        public static void Show(string message = "", string title = "")
+        public static void Show(string message = "", string title = "", string iconType = "")
         {
+            MessageBoxIcon icon = MessageBoxIcon.None;
+
+            switch (iconType)
+            {
+                case "!":
+                    icon = MessageBoxIcon.Information;
+                    break;
+
+                case "?":
+                    icon = MessageBoxIcon.Exclamation;
+                    break;
+            }
+
             Message = message;
             Title = title;
 
-            MessageBox.Show(message, title);
+            MessageBox.Show(message, title, MessageBoxButtons.OK, icon);
         }
 
-        public static bool Reply(string message, string title)
+        public static bool Reply(string message = "", string title = "")
         {
             bool reply = false;
 
-            if (MessageBox.Show(message, title, MessageBoxButtons.YesNo, 
-                MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2) 
-                == DialogResult.Yes)
+            if (MessageBox.Show(message, title, MessageBoxButtons.YesNo,
+                MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
             {
                 reply = true;
             }
