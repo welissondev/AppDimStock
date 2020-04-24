9b0e823c9ce27ced65af7cc6b551bb65c97ebb26
WelissonDev <welissondev@gmail.com>
2020-04-24 19:28:21 -0300

    Faz refaturação no código do modelo do usuário
    Volta o nome da classe como userLoginModel
    
    Remove todos os códigos para fazer uma nova implementação no modelo
    
    Cria uma classe para representar o modelo de permissão do usuário

---
+++ Projeto/Models/UserLoginModel.cs
@@ -0,0 +1,22 @@
+﻿namespace DimStock.Models
+{
+    /// <summary>
+    /// Representa o modelo de login do usuário
+    /// </summary>
+    public partial class UserLoginModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string PassWord { get; set; }
+        public UserPermissionModel Permission { get; set; }
+    }
+
+    public partial class UserLoginModel
+    {
+        public UserLoginModel()
+        {
+        }    
+    }
+}
---
+++ Projeto/Models/UserPermissionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DimStock.Models
+{
+    public class UserPermissionModel
+    {
+        public int Id { get; set; }
+        public bool Insert { get; set; }
+        public bool Update { get; set; }
+        public bool Delete { get; set; }
+        public bool Viewer { get; set; }
+    }
+}
