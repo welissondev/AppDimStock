cec633d8c196f8070a5919daa3da4e598b636e2a
WelissonDev <welissondev@gmail.com>
2020-04-25 13:25:34 -0300

    Remove classe de permissão

--- Projeto/Models/UserLoginModel.cs
+++ Projeto/Models/UserLoginModel.cs
@@ -10,13 +10,37 @@
         public string Email { get; set; }
         public string Login { get; set; }
         public string PassWord { get; set; }
-        public UserPermissionModel Permission { get; set; }
+        public bool InsertAllowed { get; set; }
+        public bool UpdateAllowed { get; set; }
+        public bool DeleteAllowed { get; set; }
     }
 
     public partial class UserLoginModel
     {
         public UserLoginModel()
         {
-        }    
+        }
+
+        public bool Insert()
+        {
+            var sql = string.Empty;
+            var actionResult = false;
+
+            using (var dataBase = new ConnectionModel())
+            {
+                sql = @"INSERT INTO UserLogin ([Name], Email, Login, 
+                [PassWord]) VALUES (@Name, @Email, @Login, @PassWord";
+
+                dataBase.ClearParameter();
+                dataBase.AddParameter("@Name", Name);
+                dataBase.AddParameter("@Email", Email);
+                dataBase.AddParameter("@Login", Login);
+                dataBase.AddParameter("PassWord", PassWord);
+
+                if (dataBase.ExecuteNonQuery(sql) > 0)
+                {
+                }
+            }
+        }
     }
 }
