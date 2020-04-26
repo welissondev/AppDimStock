0ed7c2d69c3450708c8cafaee316c533178887d0
WelissonDev <welissondev@gmail.com>
2020-04-26 12:08:30 -0300

    Adiciona apresentador de cadastro do usuário

---
+++ Projeto/Presenters/UserLoginAddPresenter.cs
@@ -0,0 +1,14 @@
+﻿using DimStock.Views;
+
+namespace DimStock.Presenters
+{
+    public class UserLoginAddPresenter
+    {
+        private IUserLoginAddView view;
+
+        public UserLoginAddPresenter(IUserLoginAddView view)
+        {
+            this.view = view;
+        }
+    }
+}
