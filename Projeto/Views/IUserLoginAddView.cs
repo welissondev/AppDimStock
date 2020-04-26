dd92008d07ddb40f9527d4024cea477b469983cc
WelissonDev <welissondev@gmail.com>
2020-04-26 12:06:15 -0300

    Adiciona interface visão de cadastro do usuário

---
+++ Projeto/Views/IUserLoginAddView.cs
@@ -0,0 +1,15 @@
+﻿namespace DimStock.Views
+{
+    public interface IUserLoginAddView
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        string Login { get; set; }
+        string AccessPassWord { get; set; }
+        string PassWordCheck { get; set; }
+        bool InsertAllower { get; set; }
+        bool UpdateAllower { get; set; }
+        bool DeleteAllower { get; set; }
+    }
+}
