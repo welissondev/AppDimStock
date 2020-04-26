47bdf5255c307906642044d82c4ad3615e5effa6
WelissonDev <welissondev@gmail.com>
2020-04-26 12:48:19 -0300

    Adiciona apresentador de listagem dos usuários

---
+++ Projeto/Presenters/UserLoginListingPresenter.cs
@@ -0,0 +1,14 @@
+﻿using DimStock.Views;
+
+namespace DimStock.Presenters
+{
+    public class UserLoginListingPresenter
+    {
+        private IUserLoginListingView view;
+
+        public UserLoginListingPresenter(IUserLoginListingView view)
+        {
+            this.view = view;
+        }
+    }
+}
