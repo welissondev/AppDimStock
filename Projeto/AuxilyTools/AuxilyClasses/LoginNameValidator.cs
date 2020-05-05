89180e1c3966abeb421b926bdbf6309f5a4e4bab
WelissonDev <welissondev@gmail.com>
2020-05-05 01:55:44 -0300

    Adiciona classe validatora do nome de login

---
+++ Projeto/AuxilyTools/AuxilyClasses/LoginNameValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DimStock.AuxilyTools.AuxilyClasses
+{
+    public class LoginNameValidator
+    {
+        public static bool CheckIfOnlyHaveNumbers(string loginName)
+        {
+            var regex = new Regex(@"[0-9]+");
+            return regex.IsMatch(loginName);
+        }
+    }
+}
