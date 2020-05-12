cd1c0eb499ef60018db012e478546cd832b57145
WelissonDev <welissondev@gmail.com>
2020-05-12 12:49:35 -0300

    Adiciona apresentador de listagem do destino

---
+++ Projeto/Presenters/DestinationListingPresenter.cs
@@ -0,0 +1,14 @@
+﻿using DimStock.Views;
+
+namespace DimStock.Presenters
+{
+    public class DestinationListingPresenter
+    {
+        private IDestinationListingView view;
+
+        public DestinationListingPresenter(IDestinationListingView view)
+        {
+            this.view = view;
+        }
+    }
+}
