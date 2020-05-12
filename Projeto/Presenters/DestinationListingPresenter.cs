06f6eb91456df0a0a928ec86d6b4aaeb49db4d40
WelissonDev <welissondev@gmail.com>
2020-05-12 13:08:47 -0300

    Adiciona apresentador de listagem do destino

--- Projeto/Presenters/DestinationListingPresenter.cs
+++ Projeto/Presenters/DestinationListingPresenter.cs
@@ -1,4 +1,5 @@
 ﻿using DimStock.Views;
+using DimStock.Models;
 
 namespace DimStock.Presenters
 {
@@ -9,6 +10,52 @@ namespace DimStock.Presenters
         public DestinationListingPresenter(IDestinationListingView view)
         {
             this.view = view;
+        }
+
+        public bool Delete()
+        {
+            var actionResult = false;
+
+            if (new DestinationModel() { Id = view.Id }.Delete() == true)
+            {
+                actionResult = true;
+                Clear();
+            }
+
+            return actionResult;
+        }
+
+        public bool ViewDetails()
+        {
+            var actionResult = false;
+
+            var destination = new DestinationModel() { Id = view.Id };
+
+            if (destination.GetDetails() == true)
+            {
+                view.LocationDescription = destination.LocationDescription;
+                actionResult = true;
+            }
+
+            return actionResult;
+        }
+
+        public void Clear()
+        {
+            view.Id = 0;
+            view.LocationDescription = string.Empty;
+
+            SearchData();
+        }
+
+        public void SearchData()
+        {
+            var destination = new DestinationModel()
+            {
+                LocationDescription = view.SearchDestinationDescription
+            };
+
+            view.DataList = destination.SearchData();
         }
     }
 }
