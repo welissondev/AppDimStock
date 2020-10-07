ca5dc22ea571f49621f3f45869e4113b9e69c237
WelissonDev <welissondev@gmail.com>
2020-10-07 10:28:03 -0300

    #59 Adiciona validação para não deletar destinos referênciados

--- Projeto/Models/DestinationModel.cs
+++ Projeto/Models/DestinationModel.cs
@@ -133,6 +133,11 @@ namespace DimStock.Models
             return actionResult;
         }
 
+        public bool CheckRelateWithMovement()
+        {
+            return new MovementModel(this).CheckRelateWithDestination();
+        }
+
         public bool CheckIfRegister()
         {
             /*Essa verificação também precisou ser feita pelo 
