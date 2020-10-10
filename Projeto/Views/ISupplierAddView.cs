a3d5b08f2246978358b8f4ff2ada0de48d06a5db
WelissonDev <welissondev@gmail.com>
2020-10-10 11:19:48 -0300

    Adiciona interface de visualização do fornecedor

---
+++ Projeto/Views/ISupplierAddView.cs
@@ -0,0 +1,21 @@
+﻿namespace DimStock.Views
+{
+    public interface ISupplierAddView
+    {
+        int Id { get; set; }
+        string TypeOfRegistration { get; set; }
+        string CompyName { get; set; }
+        string FantasyName { get; set; }
+        string IndentyCard_StateRegister { get; set; }
+        string SocialSecurity_NationalRegister { get; set; }
+        string Description { get; set; }
+        string District { get; set; }
+        string City { get; set; }
+        string StateLocation { get; set; }
+        string ZipCode { get; set; }
+        int StreetNumber { get; set; }
+        string FixePhone { get; set; }
+        string MobilePhone { get; set; }
+        string Email { get; set; }
+    }
+}
