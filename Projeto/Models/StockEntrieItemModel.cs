f5fd652212166e0c77c50923495f807ccb40c888
WelissonDev <welissondev@gmail.com>
2020-10-14 13:09:09 -0300

    Adiciona classe modelo de entrada do item

---
+++ Projeto/Models/StockEntrieItemModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Data;
+
+namespace DimStock.Models
+{
+    public class StockEntrieItemModel : ItemModel
+    {
+        public int StockEntrieId { get; set; }
+        public int ProductId{ get; set; }
+
+        public StockEntrieItemModel()
+        {
+        }
+
+        public bool Insert()
+        {
+            var actionResult = false;
+            var sql = string.Empty;
+
+            using (var dataBase = new ConnectionModel())
+            {
+                sql = @"INSERT INTO stockEntrieItem (stockEntrieId, productId, 
+                quantity, unitaryValue, totalValue)VALUES(@stockEntrieId, @productId, 
+                @quantity, @unitaryValue, @totalValue)";
+
+                dataBase.ClearParameter();
+                dataBase.AddParameter("@stockEntrieId", StockEntrieId);
+                dataBase.AddParameter("@productId", ProductId);
+                dataBase.AddParameter("@quantity", Quantity);
+                dataBase.AddParameter("@unitaryValue", UnitaryValue);
+                dataBase.AddParameter("@totalValue", TotalValue);
+
+                if (dataBase.ExecuteNonQuery(sql) > 0)
+                {
+                    actionResult = true;
+                }
+            }
+
+            return actionResult;
+        }
+
+        public bool Delete()
+        {
+            var actionResult = false;
+            var sql = string.Empty;
+
+            using (var dataBase = new ConnectionModel())
+            {
+                sql = @"DELETE FROM stockEntrieItem Where id = @id";
+
+                dataBase.ClearParameter();
+                dataBase.AddParameter("@id", Id);
+
+                if (dataBase.ExecuteNonQuery(sql) > 0)
+                {
+                    actionResult = true;
+                }
+            }
+
+            return actionResult;
+        }
+
+        public DataTable ListItems()
+        {
+            var sql = string.Empty;
+            var searchResult = new DataTable();
+
+            using (var dataBase = new ConnectionModel())
+            {
+                sql = @"SELECT i.*, p.description, p.internalCode FROM stockEntrieItem i INNER JOIN 
+                product p ON i.productId = p.id WHERE i.stockEntrieId = @stockEntrieId";
+
+                dataBase.ClearParameter();
+                dataBase.AddParameter("@stockEntrieId", StockEntrieId);
+
+                searchResult = dataBase.ExecuteDataAdapter(sql);
+            }
+
+            return searchResult;
+        }
+    }
+}
